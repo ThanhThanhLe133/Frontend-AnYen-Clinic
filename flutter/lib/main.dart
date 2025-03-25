0324357a7c4a9cfc29bb0d55fcebe70161beb415
thanh thanh le <thanhthanh.lethi133@gmail.com>
2025-03-25 14:23:36 +0700

    fix error after merge

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:anyen_clinic/chat/chat_screen.dart';
 import 'package:anyen_clinic/dashboard/dashboard.dart';
-import 'package:anyen_clinic/login/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
