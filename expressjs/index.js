37019b2a93096889d37384b8609705ec4440a52a
thanh thanh le <thanhthanh.lethi133@gmail.com>
2025-03-30 12:31:29 +0700

    setup CORS and Babel for ES6 support

--- expressjs/index.js
+++ expressjs/index.js
@@ -2,8 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth");
-
+const cors = require('cors')
 const app = express();
+app.use(cors({
+	origin: process.env.CLIENT_URL,
+	methods: ['GET', 'POST', 'PUT', 'DELETE']
+}))
+
 
 // Connect to database
 connectDB();
