0c0dab23d03d7d9fdf5e5493a5d790013065b1e6
phamdatzx <megumikatoubuhbuh@gmail.com>
2025-04-02 15:09:23 +0700

    modify sync database

--- expressjs/sync.js
+++ expressjs/sync.js
@@ -5,12 +5,14 @@ import TokenBlacklist from "./models/tokenBlacklist.js";
 import { sequelize } from "./config/sequelize.js";
 
 // Connect to database and sync models
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Database connection established successfully.");
-    return sequelize.sync({ alter: false }); // force: true will drop the table if it already exists (use cautiously)
-  })
-  .then(() => {
-    console.log("Database & tables initialized successfully.");
-  });
+export const syncDatabase = () => {
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Database connection established successfully.");
+      return sequelize.sync({ alter: false }); // force: true will drop the table if it already exists (use cautiously)
+    })
+    .then(() => {
+      console.log("Database & tables initialized successfully.");
+    });
+};
