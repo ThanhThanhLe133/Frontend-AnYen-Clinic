7af44dd3cc5b0d52095058b64302db2eff0035f5
thanh thanh le <thanhthanh.lethi133@gmail.com>
2025-05-01 19:25:17 +0700

    add show prescription and summary for patient UI

--- flutter_user/lib/dialog/Prescription.dart
+++ flutter_user/lib/dialog/Prescription.dart
@@ -1,89 +1,123 @@
+import 'dart:convert';
+
+import 'package:anyen_clinic/makeRequest.dart';
+import 'package:anyen_clinic/storage.dart';
 import 'package:anyen_clinic/widget/buildButton.dart';
 import 'package:flutter/material.dart';
 
-void showPrescriptionDialog(
-    BuildContext context, List<Map<String, String>> medicines) {
+showPrescriptionDialog(BuildContext context, String appointmentId) {
   showDialog(
     context: context,
     barrierDismissible: true,
     builder: (BuildContext context) {
-      return PrescriptionDialog(
-        medicines: medicines,
-      );
+      return PrescriptionDialog(appointment_id: appointmentId);
     },
   );
 }
 
-class PrescriptionDialog extends StatelessWidget {
-  final List<Map<String, String>> medicines;
-  const PrescriptionDialog({
-    super.key,
-    required this.medicines,
-  });
+class PrescriptionDialog extends StatefulWidget {
+  final String appointment_id;
+  const PrescriptionDialog({super.key, required this.appointment_id});
+
+  @override
+  State<PrescriptionDialog> createState() => _PrescriptionDialogState();
+}
+
+class _PrescriptionDialogState extends State<PrescriptionDialog> {
+  List<Map<String, dynamic>> prescriptions = [];
+  Future<void> fetchPrescription() async {
+    String appointmentId = widget.appointment_id;
+    final response = await makeRequest(
+      url: '$apiUrl/get/get-prescription/?appointment_id=$appointmentId',
+      method: 'GET',
+    );
+    if (response.statusCode != 200) {
+      if (!mounted) return;
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Lỗi tải dữ liệu.")));
+      Navigator.pop(context);
+    } else {
+      final data = jsonDecode(response.body);
+      setState(() {
+        prescriptions = List<Map<String, dynamic>>.from(data['data']);
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPrescription();
+  }
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     return Dialog(
-      backgroundColor: Colors.white,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       child: Padding(
         padding: EdgeInsets.all(screenWidth * 0.04),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Text(
-                  "Đơn thuốc",
-                  style: TextStyle(
-                    fontSize: screenWidth * 0.055,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue,
-                  ),
+        child: prescriptions.isEmpty
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.event_busy, size: 30.0, color: Colors.grey),
+                    SizedBox(height: 10),
+                    Text(
+                      "Không có đơn thuốc",
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(height: 10),
-              ...medicines
-                  .map((medicine) => _buildMedicineItem(medicine, screenWidth)),
-              SizedBox(height: 20),
-              Divider(height: 1),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              )
+            : Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  CustomButton(
-                    text: "ĐÓNG",
-                    isPrimary: false,
-                    screenWidth: screenWidth,
+                  Center(
+                    child: Text(
+                      "Đơn thuốc",
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.055,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
                   ),
-                  CustomButton(
-                    text: "LIÊN HỆ CSKH",
-                    isPrimary: true,
-                    screenWidth: screenWidth,
-                    onPressed: () {
-                      print("Nút được nhấn!");
-                    },
+                  SizedBox(height: 10),
+                  ...prescriptions.map(
+                    (medicine) => _buildMedicineItem(medicine, screenWidth),
+                  ),
+                  SizedBox(height: 20),
+                  Divider(height: 1),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      CustomButton(
+                        text: "ĐÓNG",
+                        isPrimary: false,
+                        screenWidth: screenWidth,
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
-        ),
       ),
     );
   }
 
-  Widget _buildMedicineItem(Map<String, String> medicine, double screenWidth) {
+  Widget _buildMedicineItem(Map<String, dynamic> medicine, double screenWidth) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(
-            "• ${medicine['name']}",
+            "• ${medicine['name_amount']}",
             style: TextStyle(
               fontSize: screenWidth * 0.04,
               fontWeight: FontWeight.bold,
@@ -92,7 +126,9 @@ class PrescriptionDialog extends StatelessWidget {
           Text(
             "  ${medicine['dosage']}",
             style: TextStyle(
-                fontSize: screenWidth * 0.035, color: Colors.grey[700]),
+              fontSize: screenWidth * 0.035,
+              color: Colors.grey[700],
+            ),
           ),
         ],
       ),
